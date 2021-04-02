23371e705c244efe9ad822aeb50243e9d344a53b
Kacper <kacpergierycz@gmail.com>
2021-04-02 13:24:58 +0200

    hacker rank practice

--- demo/src/main/java/exercise/Practice.java
+++ demo/src/main/java/exercise/Practice.java
@@ -2,6 +2,43 @@ package exercise;
 
 public class Practice {
 	
+
+
+	    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        
+        int aCount=0;
+        int sL=s.length(); 
+        
+        if (n<=sL){       
+        for(long cur=0;cur<n;cur++){         
+            if (s.charAt((int) cur)=='a'){
+                aCount++;
+            }                           
+        }        
+        return aCount; 
+        }
+        else{
+        for(long cur=0;cur<sL;cur++){            
+            if (s.charAt((int) cur)=='a'){
+                aCount++;
+            }            
+        }
+        long howMuchTimes=  n/sL;         
+        long howManyTimesA=howMuchTimes*aCount;
+        long howManyMore =n-sL*howMuchTimes;
+        int aCountTwo=0;
+            
+            for(long cur=0;cur<howManyMore;cur++){            
+                if (s.charAt((int) cur)=='a'){
+                aCountTwo++;
+            } 
+            
+        }
+        return aCountTwo+howManyTimesA;
+    }
+    }
+	
     public static int countingValleys(int steps, String path) {
     // Write your code here
 
